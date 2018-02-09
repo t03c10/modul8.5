38f360af20268b3381729f9875758a6b368842a7
Pawel Z <t03c10@gmail.com>
2018-02-09 14:11:49 +0100

    task 8.5 Finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 
-var name = prompt('Enter your name');
-//alert('Hello, ' + name);
-console.log('Hello, ' + name) // displays output window
-/*
-  Komentarz blokowy
-*/
+
+var a = prompt('Enter value for "a"'), h = prompt('Enter value for "h"'), triangleArea;
+triangleArea=a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
